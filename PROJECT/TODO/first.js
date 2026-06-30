bddf10da8628ccbc53f9a7ce98cfe3f1fe909271
Chitransh Singh <chitranshsingh02@gmail.com>
2026-07-01 01:13:49 +0530

    adding local storage to todo app

---
+++ PROJECT/TODO/first.js
@@ -0,0 +1,82 @@
+let input=document.getElementById("in");
+let but=document.getElementById("but");
+let alltodo=document.getElementById("alltodo");
+let del=document.getElementById("del");
+
+
+
+
+let data=JSON.parse(localStorage.getItem("alltodo")) || [];
+
+
+let id=1;
+
+
+
+
+function displaytodo(){
+    alltodo.innerHTML="";
+
+    data.map((singletodo,idx)=>{
+        todoStructure(singletodo),idx;
+    })
+}
+
+
+//CLICK TO ADD TODO
+but.addEventListener("click",(e)=>{
+e.preventDefault();
+let todoVal=input.value;
+data.push(todoVal);
+// todoStructure();   
+displaytodo();
+    input.value="";   
+    localStorage.setItem("alltodo",JSON.stringify(data));
+});
+
+
+
+//REMOVE TODO BY DELETE BUTTON
+function removetodo(id,idx){
+    let div2 = document.getElementById(id);
+    div2.remove();
+    removefrarr(idx);
+}
+
+
+function removefrarr(idx){
+    
+
+}
+
+
+
+// LINE-THROUGH TODO WHEN DONE
+function marktodo(id){
+        let div3 = document.getElementById(id);
+        // console.log( div3.children);
+        // div3.children[0].classList.add("completed");
+        let li = div3.children[0]; // the <li>
+    let h3 = li.children[1];   // the <h3>
+    h3.classList.toggle("completed");
+}
+
+
+
+
+
+//FUNCTION CALL TO ADD TODO TO THE DISPLAY 
+function todoStructure(singletodo,idx){
+    let value=input.value;
+    // console.log(value);
+    let div1=document.createElement("div");
+
+div1.innerHTML=` <li>
+                <input type="checkbox" id="check${id}" onclick="marktodo(${id})">
+                <h3>${singletodo}</h3>
+                <button onclick=removetodo(${id},${idx})>Delete</button>
+            </li>`
+            div1.setAttribute("id",id++)
+    alltodo.appendChild(div1); 
+}
+displaytodo();
--- practice/first.js
+++ practice/first.js
@@ -3,7 +3,33 @@ sessionStorage.setItem("sirname","singh")
 
 let name=document.getElementById("name");
 let button =document.getElementById("butt");
+let div=document.getElementById("div");
+let body=document.getElementsByTagName("body")
+let h=document.getElementById("frobj");
+let h2=document.getElementById("frobj2");
+
 button.addEventListener("click",()=>{
     let a=localStorage.getItem("name");
     name.textContent=a;
+});
+
+let input = document.getElementById("input");
+input.addEventListener("input",(e)=>{
+    localStorage.setItem("text",e.target.value);
+    div.innerHTML=`<h1>${localStorage.getItem("text")}</h1>`
 })
+let a={name:"chitrnash",rollno:"1234567890"};
+sessionStorage.setItem("object",JSON.stringify(a));
+
+let ok =document.getElementById("ok").addEventListener("click",()=>{
+    console.log("buttton working");
+    let n=JSON.parse(sessionStorage.getItem("object"));
+    let name=n.name;
+    console.log(name);
+    h.innerText=name;
+    h2.innerText=n.rollno;
+
+    
+})
+
+
