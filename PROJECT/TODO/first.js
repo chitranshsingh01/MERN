f707020fa249d95bfad63402f64a06236710b9fb
Chitransh Singh <chitranshsingh02@gmail.com>
2026-07-01 12:48:58 +0530

    todo app completed

--- PROJECT/TODO/first.js
+++ PROJECT/TODO/first.js
@@ -2,6 +2,7 @@ let input=document.getElementById("in");
 let but=document.getElementById("but");
 let alltodo=document.getElementById("alltodo");
 let del=document.getElementById("del");
+let checkbox=document.getElementById("check"); 
 
 
 
@@ -14,11 +15,12 @@ let id=1;
 
 
 
+
 function displaytodo(){
     alltodo.innerHTML="";
 
     data.map((singletodo,idx)=>{
-        todoStructure(singletodo),idx;
+        todoStructure(singletodo,idx);
     })
 }
 
@@ -27,7 +29,7 @@ function displaytodo(){
 but.addEventListener("click",(e)=>{
 e.preventDefault();
 let todoVal=input.value;
-data.push(todoVal);
+data.push({todoVal,completed:false});
 // todoStructure();   
 displaytodo();
     input.value="";   
@@ -45,20 +47,24 @@ function removetodo(id,idx){
 
 
 function removefrarr(idx){
-    
-
+    data.splice(idx,1);
+    localStorage.setItem("alltodo",JSON.stringify(data));
+    displaytodo();
 }
 
 
 
 // LINE-THROUGH TODO WHEN DONE
-function marktodo(id){
+function marktodo(id,idx){
+    
         let div3 = document.getElementById(id);
         // console.log( div3.children);
         // div3.children[0].classList.add("completed");
         let li = div3.children[0]; // the <li>
     let h3 = li.children[1];   // the <h3>
     h3.classList.toggle("completed");
+    data[idx].completed=!(data[idx].completed);
+    localStorage.setItem("alltodo",JSON.stringify(data));
 }
 
 
@@ -72,11 +78,18 @@ function todoStructure(singletodo,idx){
     let div1=document.createElement("div");
 
 div1.innerHTML=` <li>
-                <input type="checkbox" id="check${id}" onclick="marktodo(${id})">
-                <h3>${singletodo}</h3>
+                <input type="checkbox" id="check${id}" onclick="marktodo(${id},${idx})">
+                <h3>${singletodo.todoVal}</h3>
                 <button onclick=removetodo(${id},${idx})>Delete</button>
             </li>`
             div1.setAttribute("id",id++)
     alltodo.appendChild(div1); 
+    let checkbox = div1.querySelector("input");
+let h3 = div1.querySelector("h3");
+
+if (singletodo.completed) {
+    checkbox.checked = true;
+    h3.classList.add("completed");
+}
 }
 displaytodo();
